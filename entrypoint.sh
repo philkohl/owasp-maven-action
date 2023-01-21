fe1e42a107a671142b30e6af45520a97ad93fa0e
philkohl <daniel.philipp.kohl@gmail.com>
2023-01-21 12:11:57 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ ls -l /github/home/
 echo .
 ls -l .
 
+echo pwd
+pwd
+
 mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]'
 
 mvn org.owasp:dependency-check-maven:check ${all_args}
